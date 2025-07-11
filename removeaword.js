880dbfcd83a6c02a82a9708b2d6b330845a1acdb
何贤君 <56392796+yihngwan@users.noreply.github.com>
2025-07-11 14:49:43 +0800

    Update removeaword.js

--- removeaword.js
+++ removeaword.js
@@ -1,23 +1,20 @@
 // ==UserScript==
-// @name         移除考研真题点击单词跳转
+// @name         移除真题点击单词弹窗
 // @namespace    http://tampermonkey.net/
 // @version      1.0
-// @description  尝试移除网页上的点击事件监听器
+// @description  尝试移除英语考研真题上的点击单词的弹窗，减少误触提高做题时候的专注
 // @author       yihngwan
 // @match        *://zhenti.burningvocabulary.cn/*
 // @grant        none
 // @icon         https://zhenti.burningvocabulary.cn/favicon-16x16.png
-// @homepage     http://github.com/yihngwan/removeAWORD
-// @updateURL    https://raw.githubusercontent.com/yihngwan/removeAWORD/refs/heads/main/removeaword.js
-// @downloadURL  https://raw.githubusercontent.com/yihngwan/removeAWORD/refs/heads/main/removeaword.js
-
+// @homepage     https://gist.github.com/yihngwan/e8dd03a4996acc7f3d02c5f94394c9cf
+// @updateURL    https://gist.githubusercontent.com/yihngwan/e8dd03a4996acc7f3d02c5f94394c9cf/raw/remove-aword-popup.js
+// @downloadURL  https://gist.githubusercontent.com/yihngwan/e8dd03a4996acc7f3d02c5f94394c9cf/raw/remove-aword-popup.js
 // ==/UserScript==
-(function() {
-   // 'use strict';
 
+(function() {
     document.documentElement.addEventListener('click', function(e) {
         const t = e.target;
-
         // 精确拦截特定条件下的查词逻辑：AWORD 标签 或 class="aword"
         if (t.tagName.toUpperCase() === 'AWORD' || t.classList.contains('aword')) {
             e.stopImmediatePropagation();
