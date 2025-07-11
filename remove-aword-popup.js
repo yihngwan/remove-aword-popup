13d49ee2ae075f89cf22834cecad3bcb054de5a6
何贤君 <56392796+yihngwan@users.noreply.github.com>
2025-07-11 15:08:11 +0800

    Update remove-aword-popup.js

--- remove-aword-popup.js
+++ remove-aword-popup.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         移除真题点击单词弹窗
-// @namespace    http://tampermonkey.net/
-// @version      1.0b
+// @namespace    https://github.com/yihngwan/
+// @version      1.0
 // @description  尝试移除英语考研真题上的点击单词的弹窗，减少误触提高做题时候的专注
 // @author       yihngwan
 // @match        *://zhenti.burningvocabulary.cn/*
